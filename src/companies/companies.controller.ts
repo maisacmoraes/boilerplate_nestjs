e2575ef750591db1449eb1fdbc440edafb63eea8
maisacmoraes <maisa.moraes@outlook.com>
2025-06-08 17:11:13 -0300

    Adds async/await to controller methods for consistency
    Refactors all controller methods to use async/await for better
    readability and consistency across asynchronous operations.
    
    Improves code style by reformatting import statements.

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
@@ -8,27 +16,30 @@ export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
   @Post()
-  create(@Body() createCompanyDto: CreateCompanyDto) {
-    return this.companiesService.create(createCompanyDto);
+  async create(@Body() createCompanyDto: CreateCompanyDto) {
+    return await this.companiesService.create(createCompanyDto);
   }
 
   @Get()
-  findAll() {
-    return this.companiesService.findAll();
+  async indAll() {
+    return await this.companiesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.companiesService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.companiesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
-    return this.companiesService.update(+id, updateCompanyDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateCompanyDto: UpdateCompanyDto
+  ) {
+    return await this.companiesService.update(+id, updateCompanyDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.companiesService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.companiesService.remove(+id);
   }
 }
